0597b3794407493972bb61cfd18227ec8d913f41
Dayton Hare <Owner@Daytons-iMac.local>
2024-05-19 13:16:05 -0400

    added other youtube links

--- js/loadMusic.js
+++ js/loadMusic.js
@@ -29,9 +29,9 @@ $.getJSON("../jsonFiles/compositions.json", function(data) {
     var cardHtml = `
       <div class="cell" data-equalizer-watch>
         <div class="card">
-          <img src="${item.scoreImageLoc}" alt="Score Image">
-          <div class="card-section">
-            <h4>${item.title}</h4>
+        ${item.scoreImageLoc ? `<img src="${item.scoreImageLoc}" alt="Score Image" class="thumbnail">` : ''}
+        <div class="card-section">
+            <h3>${item.title}</h3>
             <p>${item.instrumentation}</p>
             <p>${item.duration}</p>
             <button class="button" data-open="exampleModal1" data-id="${item.id}">More Info</button>
@@ -44,20 +44,37 @@ $.getJSON("../jsonFiles/compositions.json", function(data) {
     $(contentId).append(cardHtml); // Add to specific category tab
   });
 
-  // Setup click handlers for buttons to populate and show the modal
-  $(".button[data-open='exampleModal1']").on("click", function() {
+// Setup click handlers for buttons to populate and show the modal
+$(".button[data-open='exampleModal1']").on("click", function() {
     var id = $(this).data("id");
     var item = data.find(i => i.id == id);
 
     $("#modalTitle").text(item.title);
     $("#modalInstrumentation").text(item.instrumentation);
     $("#modalDuration").text(item.duration);
-    $("#modalImage").attr("src", item.scoreImageLoc);
-    $("#modalSoundCloud iframe").attr("src", `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${item.soundCloudLink}&color=%23ff5500&inverse=false&auto_play=false&show_user=true`);
-    $("#modalBuyLink").attr("href", item.youtubeLink);
+
+    if (item.scoreImageLoc) {
+        $("#modalImage").attr("src", item.scoreImageLoc).show();
+    } else {
+        $("#modalImage").hide();
+    }
+
+    if (item.soundCloudLink) {
+        $("#modalSoundCloud iframe").attr("src", `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${item.soundCloudLink}&color=%23ff5500&inverse=false&auto_play=false&show_user=true`).show();
+    } else {
+        $("#modalSoundCloud").hide();
+    }
+
+    if (item.youtubeLink) {
+        $("#modalYouTube").html(`<iframe width="420" height="315" src="https://www.youtube.com/embed/${item.youtubeLink}" frameborder="0" allowfullscreen></iframe>`).show();
+    } else {
+        $("#modalYouTube").hide();
+    }
+
+    $("#modalBuyLink").attr("href", `mailto:dayton@example.com?subject=Inquiry about ${item.title}`);
 
     $('#exampleModal1').foundation('open');
-  });
+});
 
   $(document).foundation(); // Reinitialize Foundation after dynamic content is added
 });
